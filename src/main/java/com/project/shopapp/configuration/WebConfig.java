efea8f4fdcdb4165ca45882dbdfa5caa6d6c009a
128lvt <128vanthoai@gmail.com>
2024-11-14 17:54:56 +0700

    update

--- src/main/java/com/project/shopapp/configuration/WebConfig.java
+++ src/main/java/com/project/shopapp/configuration/WebConfig.java
@@ -1,12 +1,10 @@
 package com.project.shopapp.configuration;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -16,10 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")  // Chấp nhận cả 2 URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+
 }
 
